3e1e87ba702c50dc91a456716fd994841379fe50
Emma_Behr <emp.behr@gmail.com>
2021-06-07 21:25:33 +0100

    Day 1 fizzbuzz implemented

---
+++ Day 1 - Fizz Buzz/fizzbuzz.py
@@ -0,0 +1,18 @@
+# Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+
+number = 1 
+while number <= 100:
+    if (number % 3  == 0) and (number % 5 == 0):
+        print("fizzbuzz")
+    elif number % 3 == 0:
+        print("fizz")
+    elif number % 5 == 0:
+        print("buzz")
+    else:
+        print(number)
+    number = number + 1
+
+
+
